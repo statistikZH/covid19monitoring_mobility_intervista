4e40dce62b316b507a5ee4541b7ab5301f541bd6
Peter Moser <peterjamoser@bluewin.ch>
2020-03-25 12:52:29 +0100

    reformatting intervista provisional done

---
+++ ReformatIntervistaPaneldata.R
@@ -0,0 +1,78 @@
+# import libraries
+library(tidyverse)
+library(reshape2)
+options(scipen = 1000000)
+options(digits = 6)
+
+
+dir_name <- "./Download"
+#dir.create(dir_name)
+
+# Metadaten
+downloader::download("https://www.intervista.ch/media/2020/03/Download_Mobilit%C3%A4ts-Monitoring_Covid-19.zip",
+                     dest=paste0(dir_name,"/Download.zip"), 
+                     mode="wb")
+unzip(paste0(dir_name,"/Download.zip"), exdir = dir_name)
+# import data
+dat <-  read.csv("./Download/Mittelwerte_und_Median_pro_Tag.csv", header=T, sep=",", stringsAsFactors=FALSE, encoding="ANSI_X3.4-1986") 
+
+# long format with first three vars staying
+dat2<-reshape2::melt(dat, id.vars=1:3)
+#Regional vars subsetted
+datreg<-droplevels(subset(dat2, variable%in%c("Kanton_Zürich_Ja", "Kanton_Zürich_Nein", "Ländlich", "Städtisch", "Total")))
+#pasting variable name from tages , Beschreibung (rad/dist) Typ (mean/median)
+
+
+datreg$variable2<-with(datreg, tolower(paste("tages", Beschreibung, Typ, sep="_")))
+#spatial reference in location
+datreg$location=datreg$variable
+# data values for the whole of switzerland, socially segmented
+
+datch<-droplevels(subset(dat2, variable%in%c("Alter_15.29","Alter_30.64", "Alter_65.79", "Männlich", "Weiblich", "Erwebstätig", "In_Ausbildung", "Nicht_Erwerbstätig")))
+datch$variable2<-with(datch, tolower(paste("tages", Beschreibung, Typ, variable, sep="_")))
+datch$location<-"CH"
+#recode location
+
+#values for all of switzerland, without social segments
+dattot<-droplevels(subset(dat2, variable%in%c("Total")))
+dattot$variable2<-with(dattot, tolower(paste("tages", Beschreibung, Typ, sep="_")))
+dattot$location<-"CH"
+#bind them all together
+datall<-rbind(datreg, datch, dattot)
+datall$unit<-"km"
+
+datall$location<-recode_factor(datall$location, "Kanton_Zürich_Ja" = "ZH",
+                                "Kanton_Zürich_Nein" = "CH ohne ZH",
+                                "Städtisch" = "Städtischer Raum",
+                                "Ländlich"  = "Ländlicher Raum",
+                                "CH" = "CH", 
+                                "Total" = "CH")
+
+#Long and german variablenames merge
+codvars<-read.csv("coding_vars_intervista.csv",  fileEncoding = "UTF-8")
+datall<-merge(datall, codvars, by.x="variable2", by.y="var")
+
+#Create the final dataset
+
+intervista<-data.frame(date=as.POSIXct(paste(datall$Datum, "00:00:00", sep=" ")),
+           value=datall$value,
+           topic="Mobilität",
+           variable_short=datall$variable2,
+           variable_long=datall$variable_long,
+           location=datall$location,
+           unit="km",
+           source="intervista Tracking-panel",
+           update="täglich",
+           public="ja",
+           description="https://github.com/statistikZH/covid19monitoring_mobility_intervista")
+
+#only median values and without restschweiz
+mobility_intervista<-subset(intervista, grepl("median", intervista$variable_short)==T & location!="CH ohne ZH")
+
+
+
+           )
+
+
+
+

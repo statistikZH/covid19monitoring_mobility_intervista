14631a262f4bcb5d1dbbdb5cb557f20f6a1e99c0
Peter Moser <peterjamoser@bluewin.ch>
2020-11-04 14:28:51 +0100

    included more items

--- ReformatIntervistaPaneldata.R
+++ ReformatIntervistaPaneldata.R
@@ -14,25 +14,28 @@ downloader::download("https://www.intervista.ch/media/2020/03/Download_Mobilit%C
 unzip(paste0(dir_name,"/Download.zip"), exdir = dir_name)
 # import data
 dat <-  read.csv("./Download/Mittelwerte_und_Median_pro_Tag.csv", header=T, sep=",", stringsAsFactors=FALSE, encoding="ANSI_X3.4-1986") 
-dat<-dat[,1:16]
+dat<-dat
 #remove umlauts from colnames
 colnames(dat)<-sub("ä", "ae", colnames(dat), ignore.case = T)
 colnames(dat)<-sub("ü", "ue", colnames(dat), ignore.case = T)
 colnames(dat)<-sub("ö", "oe", colnames(dat), ignore.case = T)
 colnames(dat)<-sub(".", "", colnames(dat), fixed=T)
+colnames(dat)<-sub("..", "_", colnames(dat), fixed=T)
 # long format with first three vars staying
 dat2<-reshape2::melt(dat, id.vars=1:3)
 #Regional vars subsetted
-datreg<-droplevels(subset(dat2, variable%in%c("Kanton_Zuerich_Ja", "Kanton_Zuerich_Nein", "Laendlich", "Staedtisch")))
+datreg<-droplevels(subset(dat2, variable%in%c("Kanton_Zuerich_Ja", "Kanton_Zuerich_Nein", "Laendlich", "Staedtisch", "DCH", "FCH", "ICH")))
 #pasting variable name from tages , Beschreibung (rad/dist) Typ (mean/median)
 datreg$variable2<-with(datreg, tolower(paste("tages", Beschreibung, Typ, sep="_")))
 #spatial reference in location
-datreg$location=datreg$variable
+datreg$location<-datreg$variable
+
 # data values for the whole of switzerland, socially segmented
-datch<-droplevels(subset(dat2, variable%in%c("Alter_1529","Alter_3064", "Alter_6579", "Maennlich", "Weiblich", "Erwerbstaetig", "In_Ausbildung", "Nicht_Erwerbstaetig")))
+datch<-droplevels(subset(dat2, variable%in%c("Alter_1529","Alter_3064", "Alter_6579", "Maennlich", "Weiblich", "Erwerbstaetig", "In_Ausbildung", "Nicht_Erwerbstaetig", "Abotyp_GA", "Abotyp_HalbtaxStrecken..Verbundabo", "Abotyp_kein_Abo", 
+                                             "Auto_Ja", "Auto_Nein", "Haushaltsgroesse_1_Person", "Haushaltsgroesse_2_Personen", 
+                                             "Haushaltsgroesse_3_Personen", "Kinder_Ja", "Kinder_Nein")))
 datch$variable2<-with(datch, tolower(paste("tages", Beschreibung, Typ, variable, sep="_")))
 datch$location<-"CH"
-#recode location
 
 #values for all of switzerland, without social segments
 dattot<-droplevels(subset(dat2, variable%in%c("Total")))
@@ -42,23 +45,13 @@ dattot$location<-"CH"
 datall<-rbind(datreg, datch, dattot)
 datall$unit<-"km"
 
-#location
-#c("Kanton_Zuerich_Ja", "Kanton_Zuerich_Nein", "Staedtisch", "Laendlich", "CH")
-
-levels(datall$location)<-c("ZH", "CH ohne ZH", "CH: Städtischer Raum", "CH: Ländlicher Raum", "CH")
-
-#datall$location<-recode_factor(datall$location, "Kanton_Zuerich_Ja" = "ZH",
-#                               "Kanton_Zuerich_Nein" = "CH ohne ZH",
-#                               "Staedtisch" = "CH: Städtischer Raum",
-#                               "Laendlich"  = "CH: Ländlicher Raum",
-#                               "CH" = "CH", 
-#                               "Total" = "CH")
+levels(datall$location)<-c("ZH", "CH ohne ZH", "CH: Städtischer Raum", "CH: Ländlicher Raum", "Deutschschweiz", "Romandie", "italienische Schweiz", "CH")
 
 #Long and german variablenames merge
 codvars<-read.csv("coding_vars_intervista.csv",  fileEncoding = "UTF-8")
 datall<-merge(datall, codvars, by.x="variable2", by.y="var", all.x=T)
 
-#Create the final dataset
+#Create the first part of the dataset
 
 intervista<-data.frame(date=as.Date(datall$Datum),
                        value=datall$value,
@@ -76,9 +69,6 @@ intervista<-data.frame(date=as.Date(datall$Datum),
 mobility_intervista<-droplevels(subset(intervista, grepl("distanz", intervista$variable_short)==T))
 mobility_intervista<-droplevels(subset(mobility_intervista, location!="CH ohne ZH"))
 mobility_intervista<-mobility_intervista[order(mobility_intervista$date),]
-
-
-
 
 
 # Distanzkategorien----
@@ -153,13 +143,44 @@ intervista_distkat<-data.frame(date=as.Date(datall$Datum),
 
 
 
-#write the final file for publication
 
+##
+#Modal und zwecksplit
+
+# import data
+zweck <-  read.csv("./Download/Mobilit„tszweck_pro_Tag.csv", header=T, sep=",", stringsAsFactors=FALSE, encoding="ANSI_X3.4-1986") 
+names(zweck)<-c("date", "total", "tages_distanz_mittelwert_zweck_pendeln", "tages_distanz_mittelwert_zweck_freizeit", "tages_distanz_mittelwert_zweck_einkauf", "tages_distanz_mittelwert_zweck_andere")
+zweck<-melt(zweck, id.vars="date")
+
+
+mode <-  read.csv("./Download/Modalsplit_pro_Tag.csv", header=T, sep=",", stringsAsFactors=FALSE, encoding="ANSI_X3.4-1986") 
+names(mode)<-c("date", "total", "tages_distanz_mittelwert_modus_miv", "tages_distanz_mittelwert_modus_oev", "tages_distanz_mittelwert_modus_fuss", "tages_distanz_mittelwert_modus_andere_vkm")
+
+mode<-melt(mode, id.vars="date")
+
+zwmod<-rbind(zweck, mode)
+zwmod<-droplevels(subset(zwmod, variable!="total"))
+zwmodvars<-read.csv("zweck_mod_vars.csv",  fileEncoding = "UTF-8")
+zwmod<-merge(zwmod, zwmodvars, by.x="variable", by.y="var", all.x=T)
+
+intervista_zwmod<-data.frame(date=as.Date(zwmod$date),
+                               value=zwmod$value,
+                               topic="Mobilität",
+                               variable_short=zwmod$variable,
+                               variable_long=zwmod$variable_long,
+                               location="CH",
+                               unit="km",
+                               source="intervista Tracking-panel",
+                               update="täglich",
+                               public="ja",
+                               description="https://github.com/statistikZH/covid19monitoring_mobility_intervista")
+
+#write the final file for publication
 intervista_distkat<-droplevels(subset(intervista_distkat, grepl("alter", intervista_distkat$variable_short)==T | nchar(as.character(intervista_distkat$variable_short))<25))
 intervista_distkat<-droplevels(subset(intervista_distkat, location=="CH"))
 intervista_distkat<-intervista_distkat[order(intervista_distkat$date),]
 
-mobility_intervista<-rbind(mobility_intervista, intervista_distkat)
+mobility_intervista<-rbind(mobility_intervista, intervista_distkat, intervista_zwmod)
 
 
 write.table(mobility_intervista, "Mobility_intervista.csv", sep=",", fileEncoding="UTF-8", row.names = F)
